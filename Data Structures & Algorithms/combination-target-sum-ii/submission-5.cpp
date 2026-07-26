3ad5203a62052bdf430a08ac402e527bd2f2ec77
ViratMehta1729 <77613834+ViratMehta1729@users.noreply.github.com>
2026-07-26 12:15:46 +0530

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void comb(vector<int> &v, int index, int target, vector<int> &temp)
+    {
+        if(target==0)
+        {
+            ans.push_back(temp);
+            return ;
+        }
+        if(target<0) return ;
+        for(int i = index; i< v.size(); i++)
+        {
+            if( i>index&& v[i]==v[i-1])continue;
+            temp.push_back(v[i]);
+            comb(v, i+1, target-v[i], temp);
+            temp.pop_back();
+        }
+        return ;
+    }
+    vector<vector<int>> combinationSum2(vector<int>& v, int target) {
+        sort(v.begin(), v.end());
+        vector<int> temp;
+        comb(v, 0, target, temp);
+        return ans;
+        
+    }
+};
