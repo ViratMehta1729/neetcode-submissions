937ac3dce9aa6e94fa2b6053da44e3de22d46809
ViratMehta1729 <77613834+ViratMehta1729@users.noreply.github.com>
2026-07-30 11:49:06 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,16 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int limit;
+    KthLargest(int k, vector<int>& nums) {
+        limit = k;
+        for(auto x: nums)pq.push(x);
+        while(pq.size()>k)pq.pop();
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>limit)pq.pop();
+        return pq.top();
+    }
+};
