9a48a222682907cb050d9665f33998a28495c50a
ViratMehta1729 <77613834+ViratMehta1729@users.noreply.github.com>
2026-08-24 12:42:22 +0530

    Add: target-sum - submission-5

---
+++ Data Structures & Algorithms/target-sum/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum =0;
+        for(auto x: nums)sum+=x;
+        
+        int k = sum+target;
+        if(k<0) return  0;
+        k = abs(k);
+        if(k&1) return 0;
+        k/=2;
+        vector<int> dp(k+1, 0);
+        dp[0] = 1;
+        for(auto x: nums)
+        {
+            for(int i = k; i>=x; i--)
+            {
+                dp[i]+=dp[i-x];
+            }
+        }
+        return dp[k];
+        
+    }
+};
